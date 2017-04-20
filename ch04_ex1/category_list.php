e2bb163af6a789f672735bd0ef6e810219d5d06f
sravani kanchanapalli <sk752@afsconnect1.njit.edu>
2017-04-20 10:49:32 -0400

    added categoryID

--- ch04_ex1/category_list.php
+++ ch04_ex1/category_list.php
@@ -30,7 +30,18 @@ $statement->closeCursor();
         </tr>
         
         <!-- add code for the rest of the table here -->
-    
+    	<?php foreach ($categories as $category) : ?>
+	<tr>
+		<td> <?php echo $category ['categoryName']; ?> </td>
+		<td> <form action = "delete_category.php" method = "post">
+		  <input type + "hidden" name = "category_id" />
+		    value = "<?php echo $category ['categoryID'];
+		  <input type = "submit" value = "Delete" /> 
+		  <?form>
+		</td>
+	</tr>
+	<?php endforeach; ?>
+
     </table>
 
     <h2>Add Category</h2>
